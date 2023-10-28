c1a725db8159bf78d383bee65e03816e7706e1c2
Pragadesh BS <pragadeshbs@protonmail.com>
2023-10-28 09:30:13 +0530

    replace cra with vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { AuthContextProvider } from "./contexts/AuthContext";
 import "./index.css";
 import App from "./App";
-import { AuthContextProvider } from "../src/contexts/AuthContext";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
       <App />
